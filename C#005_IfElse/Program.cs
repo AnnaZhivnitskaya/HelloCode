14ef0cc091209023b0a3b0b77567d5233a584b3c
Anna <A9183307737@gmail.com>
2022-11-21 10:20:10 +0300

    end C#005

---
+++ C#005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Напишите свое имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+ Console.WriteLine("Так это же Маша!");
+}
+else
+{
+ Console.Write("Привет, ");
+ Console.WriteLine(username);
+}
