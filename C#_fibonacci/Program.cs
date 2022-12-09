e00aeae16001995a82db39115a724d52acdbac45
Anna <A9183307737@gmail.com>
2022-12-09 18:10:56 +0300

    fibonacci

---
+++ C#_fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿// Нахождение числа Фибоначчи через рекурсию
+
+// f(1) = 1
+// f(2) = 2 
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2)
+        return 1;
+
+    else
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+{
+    for (int i = 1; i <= 8; i++)
+    Console.WriteLine($"{i!} {Fibonacci(i)}");
+}
+
+
+
+//Console.WriteLine(Fibonacci(8));
+
+
+
